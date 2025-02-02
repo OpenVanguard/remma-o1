6a74c01bbb326c832c9ffad0444b1540fda0ace2
ViratSrivastava <vs.viratsrivastava11111@gmail.com>
2025-02-02 23:20:58 +0530

    Create __init__.py

---
+++ src/modeling/__init__.py
@@ -0,0 +1,12 @@
+from .transformer import TransformerModel
+
+def build_model(config):
+    return TransformerModel(
+        vocab_size=config["vocab_size"],
+        embed_size=config["embed_size"],
+        num_layers=config["num_layers"],
+        num_heads=config["num_heads"],
+        ff_dim=config["ff_dim"],
+        block_size=config["block_size"],
+        dropout=config["dropout"]
+    )
